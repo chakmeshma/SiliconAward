b259de9e65ca4369f0fa330ab88f80a59c6b12d5
Majid Motallebikashani <chakmeshma@gmail.com>
2019-09-25 11:04:20 +0330

    Register Added HTML and CSS and JS

---
+++ SiliconAward/wwwroot/js/new js/script.js
@@ -0,0 +1,38 @@
+var bindLoginContainerResizeHandler = function() {
+
+
+    var windowHeight = window.innerHeight;
+    var windowWidth = window.innerWidth;
+    var elementHeight = $(".loginContainer").height();
+    var elementWidth = $(".loginContainer").width();
+    var topMargin = (windowHeight - elementHeight) / 2;
+
+    $(".loginContainer").css("marginTop", "" + topMargin + "px");
+};
+
+function init() {
+    $(window).ready(bindLoginContainerResizeHandler);
+    $(window).resize(bindLoginContainerResizeHandler);
+
+}
+
+function togglePasswordView() {
+    var current = $("#passwordField").attr("type");
+
+    if (current == "password")
+        $("#passwordField").attr("type", "text")
+    else
+        $("#passwordField").attr("type", "password")
+}
+
+function initInputEvents() {
+    $(".code-entry-input").click(function() {
+        $(this).select();
+    });
+    $(".code-entry-input").keyup(function() {
+        if (this.value.length == this.maxLength) {
+            $(this).next('.code-entry-input').focus();
+            $(this).next('.code-entry-input').select()
+        }
+    });
+}
