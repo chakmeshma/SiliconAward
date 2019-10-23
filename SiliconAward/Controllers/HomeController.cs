1e3987519d95ebc6c8c67fa964bd2d0c0799bcc9
Majid Motallebikashani <chakmeshma@gmail.com>
2019-10-23 14:24:26 +0330

    Challenge Select WIP

--- SiliconAward/Controllers/HomeController.cs
+++ SiliconAward/Controllers/HomeController.cs
@@ -48,5 +48,10 @@ namespace SiliconAward.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult ChallengeSelect()
+        {
+            return View();
+        }
     }
 }
